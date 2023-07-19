dcb616334e334eaae98ba276cd83cab729224e01
Krishnamoorthy Natarajan <krishna@Krishnamoorthys-MacBook-Pro.local>
2023-07-19 22:39:40 +0530

    Initial commit

---
+++ src/main/java/com/thoughtworks/profiler/ProfilerApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.profiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilerApplication.class, args);
+	}
+
+}
